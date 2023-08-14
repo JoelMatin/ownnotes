4d0558a86cddbedecc4e8776eda31fcbfe70f0db
PachaAfghan <joel.matin2@gmail.com>
2023-08-15 00:56:27 +0200

    step-2 bis

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:ownnotes/views/login_view.dart';
 import 'package:ownnotes/views/register_view.dart';
+import 'package:ownnotes/views/verify_email_view.dart';
 
 import 'firebase_options.dart';
 
@@ -34,16 +35,17 @@ class HomePage extends StatelessWidget {
         //snapshot is the way to get the result of the future
         switch (snapshot.connectionState) {
           case ConnectionState.done:
-            // final user = FirebaseAuth.instance.currentUser;
-            // if (user?.emailVerified ?? false) {
-            //   return const Text("Done");
-            // } else {
-            //   print("you need to verify your email");
-            //   Navigator.of(context).push(MaterialPageRoute(
-            //       builder: (context) => VerifyEmailView()));
-            // }
-            // return const Text("Done");
-            return const LoginView();
+            final user = FirebaseAuth.instance.currentUser;
+            if (user != null) {
+              if (user.emailVerified) {
+                print("Email is verified ");
+              } else {
+                return const VerifyEmailView();
+              }
+            } else {
+              return const LoginView();
+            }
+            return const Text("Done");
           default:
             return const CircularProgressIndicator();
         }
